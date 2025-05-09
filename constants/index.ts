f112c92796a6ff38442e71830a9a34b8758ea7e1
Aki Inoyatov <info@gravityelements.com>
2025-05-08 22:40:15 -0700

    initial setup

---
+++ constants/index.ts
@@ -0,0 +1,25 @@
+export const NavLinks = [
+    {href:"Services", label:"/services"},
+    {href:"Gallery", label:"/gallery"},
+    {href:"About Us", label:"/about-us"},
+    {href:"Contact Us", label:"/contact-us"},
+]
+
+export const FooterLinks = [
+    {
+        title: "COMPANY",
+        links: [
+            {href:"Services", label:"/services"},
+            {href:"Gallery", label:"/gallery"},
+            {href:"Prices", label:"/prices"},
+        ]
+    },
+    {
+        title: "CONTACT",
+        links: [
+            {href:"Appointment", label:"/appointment"},
+            {href:"FAQ", label:"/faq"},
+            {href:"Contact Us", label:"/contact-us"},
+        ]
+    },
+]

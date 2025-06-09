7b92465503ce34a9ad5e5f003159bc794957a043
Aki Inoyatov <info@gravityelements.com>
2025-06-08 22:54:28 -0700

    new contact form

---
+++ components/forms/ContactForm.tsx
@@ -0,0 +1,101 @@
+'use client';
+
+import { useState, useEffect } from "react";
+import ReCAPTCHA from 'react-google-recaptcha';
+
+export type FormDataPoints = {
+    full_name?: string,
+    email?: string,
+    phone?: string,
+    message?: string
+};
+
+const ContactForm = () => {
+
+    const [state, setState] = useState<Partial<FormDataPoints>>({});
+    const [captcha, setCaptcha] = useState<string | null>(null);
+    const [isToggled, setIsToggled] = useState(false);
+
+    // Enable submit button once captcha is passed
+    useEffect(() => {
+        setIsToggled(!!captcha); //same as setIsToggled(captcha !== null);
+    }, [captcha]);
+
+    const FormAction = async (formData: FormData) => {
+
+        if (captcha) {
+            const payload: FormDataPoints = {
+                full_name: formData.get('full-name') as string,
+                email: formData.get('email') as string,
+                phone: formData.get('phone') as string,
+                message: formData.get('message') as string,
+            };
+
+            const verifyCaptcha = await fetch('/api/recaptcha', {
+                method: 'POST',
+                headers: { 'Content-Type': 'application/json' },
+                body: JSON.stringify({ captcha }),
+            });
+
+            const { success: captchaSuccess, message: captchaMessage } = await verifyCaptcha.json();
+
+            if (!captchaSuccess) {
+                alert(captchaMessage || 'Captcha verification failed.');
+                return;
+            }
+
+            const sendMail = await fetch('/api/contact', {
+                method: 'POST',
+                headers: { 'Content-Type': 'application/json' },
+                body: JSON.stringify(payload),
+            });
+
+            const { success, message } = await sendMail.json();
+
+            if (success) {
+                setState(payload);
+                alert(message);
+                window.location.reload();
+            } else {
+                alert(message);
+            }
+        } else {
+            alert('Please verify Captcha.');
+        }
+    };
+
+    return (
+        <form action={FormAction}>
+            <div className="flex-1 flex justify-center p-4">
+                <div className="p-8 rounded-lg shadow-lg w-full max-w-md bg-primary dark:bg-slate-500 bg-opacity-70">
+                    <div className="space-y-4">
+                        <input type="text" name="full-name" placeholder='Full Name' className="w-full p-3 rounded-md border border-gray-300 text-black placeholder-gray-600 dark:placeholder-gray-300" />
+                        <input type="email" name="email" placeholder='Email' className="w-full p-3 rounded-md border border-gray-300 text-black placeholder-gray-600 dark:placeholder-gray-300" />
+                        <input type="text" name="phone" placeholder='Phone' className="w-full p-3 rounded-md border border-gray-300 text-black placeholder-gray-600 dark:placeholder-gray-300" />
+                        <textarea name="message" placeholder='Message' className="w-full p-3 rounded-md border border-gray-300 text-black placeholder-gray-600 h-24 resize-none dark:placeholder-gray-300" />
+
+                        {/* <div className="mt-4">
+                            <ReCAPTCHA 
+                                sitekey={process.env.NEXT_PUBLIC_RECAPTCHA!}
+                                className="w-full mt-4"
+                                onChange={setCaptcha}
+                            />
+                        </div> */}
+
+                        <div className="py-2">
+                            <button
+                                disabled={!isToggled}
+                                type="submit"
+                                className={`w-full p-3 rounded-md text-white font-bold transition ${isToggled ? 'bg-black hover:bg-gray-800' : 'bg-black opacity-50 cursor-not-allowed'}`}
+                            >
+                                Get In Touch
+                            </button>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </form>
+    );
+};
+
+export default ContactForm;

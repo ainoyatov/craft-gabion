efb2ddc260482bdeffccbfb9b7fb365c6a15d68b
Aki Inoyatov <info@gravityelements.com>
2025-06-17 10:41:03 -0700

    gallery images updated

--- components/gallery/galleryData.ts
+++ components/gallery/galleryData.ts
@@ -1,22 +1,39 @@
 export const galleryData = [
   {
     slug: 'gallery-pic-1',
-    title: 'Modern Gabion Wall',
+    title: 'Modern Designs',
     thumbnail: '/gallery/gallery-main/placeholder_1.jpg',
     images: [
-      { src: '/gallery/gallery-pic-1/image1.jpg', alt: 'Modern Gabion Wall - Image 1' },
-      { src: '/gallery/gallery-pic-1/image2.jpg', alt: 'Modern Gabion Wall - Image 2' },
-      { src: '/gallery/gallery-pic-1/image3.jpg', alt: 'Modern Gabion Wall - Image 2' },
+      { src: '/gallery/gallery-pic-1/image1.jpg', alt: 'Modern Designs image 1' },
+      { src: '/gallery/gallery-pic-1/image2.jpg', alt: 'Modern Designs image 2' },
+      { src: '/gallery/gallery-pic-1/image3.jpg', alt: 'Modern Designs image 3' },
+      { src: '/gallery/gallery-pic-1/image4.jpg', alt: 'Modern Designs image 4' },
     ],
   },
   {
     slug: 'gallery-pic-2',
-    title: 'Water Feature Wall',
+    title: 'Outdoor Designs',
     thumbnail: '/gallery/gallery-main/placeholder_2.jpg',
     images: [
-      { src: '/gallery/gallery-pic-2/image4.jpg', alt: 'Water Feature Wall - Image 1' },
-      { src: '/gallery/gallery-pic-2/image5.jpg', alt: 'Water Feature Wall - Image 2' },
-      { src: '/gallery/gallery-pic-2/image6.jpg', alt: 'Water Feature Wall - Image 2' },
+      { src: '/gallery/gallery-pic-2/image4.jpg', alt: 'Outdoor Designs image 4' },
+      { src: '/gallery/gallery-pic-2/image5.jpg', alt: 'Outdoor Designs image 5' },
+      { src: '/gallery/gallery-pic-2/image6.jpg', alt: 'Outdoor Designs image 6' },
+      { src: '/gallery/gallery-pic-2/image1.jpg', alt: 'Outdoor Designs image 1' },
+      { src: '/gallery/gallery-pic-2/image2.jpg', alt: 'Outdoor Designs image 2' },
+      { src: '/gallery/gallery-pic-2/image3.jpg', alt: 'Outdoor Designs image 3' },
+    ],
+  },
+  {
+    slug: 'gallery-pic-3',
+    title: 'Indoor Designs',
+    thumbnail: '/gallery/gallery-main/placeholder_3.png',
+    images: [
+      { src: '/gallery/gallery-pic-3/indoor_gabion_1.png', alt: 'Indoor Designs image 1' },
+      { src: '/gallery/gallery-pic-3/indoor_gabion_2.png', alt: 'Indoor Designs image 2' },
+      { src: '/gallery/gallery-pic-3/indoor_gabion_3.png', alt: 'Indoor Designs image 3' },
+      { src: '/gallery/gallery-pic-3/indoor_gabion_4.png', alt: 'Indoor Designs image 4' },
+      { src: '/gallery/gallery-pic-3/indoor_gabion_5.png', alt: 'Indoor Designs image 5' },
+      { src: '/gallery/gallery-pic-3/indoor_gabion_6.png', alt: 'Indoor Designs image 6' },
     ],
   },
 ]
